927aaa3d2bf8afa908acfdbc0590fa517a530708
Aladin-mf <aladinmf01@gmail.com>
2021-03-20 22:09:04 +0600

    1

---
+++ hello.js
@@ -0,0 +1,38 @@
+var day=new Date().getDay()
+switch(day){
+    case 0:
+        console.log('today is sunday')
+        break
+
+        case 1:
+        console.log('today is mon')
+        break
+
+        case 2:
+        console.log('today is tue')
+        break
+
+
+        case 3:
+        console.log('today is wed')
+        break
+
+
+        case 4:
+        console.log('today is thurs')
+        break
+
+
+        case 5:
+        console.log('today is fry')
+        break
+
+        case 6:
+        console.log('today is sat')
+        break
+
+        default:
+            console.log("not a valid number")
+}
+
+
